73a9cbe4dd093710ff39349d392e4ffdafbcb1ad
Marco Prins <marco@kaartje2go.nl>
2023-12-06 21:53:10 +0100

    idk 2

--- SingularModuleTests/LCD/LCDTest.py
+++ SingularModuleTests/LCD/LCDTest.py
@@ -13,8 +13,23 @@ display = lcd()
 
 try:
     print("Writing to display")
-    display.lcd_display_string("No time to waste", 1)  # Write line of text to first line of display
+    display.lcd_display_string("Hello", 1)  # Write line of text to first line of display
+    sleep(5)
+
+    print("Clearing display")
+    display.lcd_clear()  # Clear the display of any data
+    sleep(2)
+
+    print("Turning off backlight")
+    display.backlight(0)  # Turn off backlight
+    sleep(2)
+
+    print("Turning on backlight")
+    display.backlight(1)  # Turn on backlight
+    sleep(2)
+
     while True:
+        print("Loop")
         # Write just the time to the display
         display.lcd_display_string(str(datetime.now().time()), 2)
         # Uncomment the following line to loop with 1 sec delay
